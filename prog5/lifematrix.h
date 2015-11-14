60171e5669eff6bf76f4bac5403deb138a27e734
gheshu <gheshu@users.noreply.github.com>
2015-11-14 17:26:47 -0600

    Update lifematrix.h

--- prog5/lifematrix.h
+++ prog5/lifematrix.h
@@ -4,12 +4,11 @@
 class LifeMatrix{
 	bool* data = nullptr;
 	int width, height;
-	inline void wrap(int& x, int& y){ x=(x+width)%width; y=(y+height)%height;}
 public:
 	void init(int w, int h);
 	void alloc(int w, int h);
 	void destroy();
-	inline bool* operator()(const int x, const int y){int j=x; int k=y; wrap(j, k); return (data+j+k*width);}
+	inline bool* operator()(const int x, const int y){int j=(x+width)%width; int k=(y+height)%height; return (data+j+k*width);}
 	int nbors(int x, int y);
 };
 
